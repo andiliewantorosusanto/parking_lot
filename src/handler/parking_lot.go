187b40039f26d0c4cdc58a8f4f4faaf5a044b03c
andiliewantorosusanto <andiliesusanto@gmail.com>
2023-06-02 20:31:24 +0700

    refactor : moved handler func from main to handler package

--- src/handler/parking_lot.go
+++ src/handler/parking_lot.go
@@ -1,1 +1,52 @@
 package handler
+
+import (
+	"github.com/gorilla/mux"
+	"gitlab.mapan.io/playground/parking-lot-golang/src/entity"
+	"net/http"
+)
+
+var parkingLot = entity.NewParkingLot()
+
+func CreateParkingSlot(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	w.Write(parkingLot.SetNumberOfSlot(params["numberOfSlot"]))
+}
+
+func Reserve(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	regNumber := params["regNumber"]
+	colour := params["colour"]
+
+	w.Write(parkingLot.Reserve(regNumber, colour))
+}
+
+func Leave(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	w.Write(parkingLot.Leave(params["slotNumber"]))
+}
+
+func Status(w http.ResponseWriter, _ *http.Request) {
+	w.Write(parkingLot.Status())
+}
+
+func GetRegNumbersByColour(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	colour := params["colour"]
+
+	w.Write(parkingLot.GetRegNumbersByColour(colour))
+}
+
+func GetReservedSlotsByColour(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	colour := params["colour"]
+
+	w.Write(parkingLot.GetReservedSlotsByColour(colour))
+}
+
+func GetReservedSlotByRegNumber(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	regNumber := params["regNumber"]
+	w.Write(parkingLot.GetReservedSlotsByColour(regNumber))
+}
--- src/main.go
+++ src/main.go
@@ -2,65 +2,20 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"gitlab.mapan.io/playground/parking-lot-golang/src/entity"
+	"gitlab.mapan.io/playground/parking-lot-golang/src/handler"
 	"net/http"
 )
-
-var parkingLot = entity.NewParkingLot()
-
-func createParkingSlot(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	w.Write(parkingLot.SetNumberOfSlot(params["numberOfSlot"]))
-}
-
-func reserve(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	regNumber := params["regNumber"]
-	colour := params["colour"]
-
-	w.Write(parkingLot.Reserve(regNumber, colour))
-}
-
-func leave(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	w.Write(parkingLot.Leave(params["slotNumber"]))
-}
-
-func status(w http.ResponseWriter, _ *http.Request) {
-	w.Write(parkingLot.Status())
-}
-
-func getRegNumbersByColour(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	colour := params["colour"]
-
-	w.Write(parkingLot.GetRegNumbersByColour(colour))
-}
-
-func getReservedSlotsByColour(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	colour := params["colour"]
-
-	w.Write(parkingLot.GetReservedSlotsByColour(colour))
-}
-
-func getReservedSlotByRegNumber(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	regNumber := params["regNumber"]
-	w.Write(parkingLot.GetReservedSlotsByColour(regNumber))
-}
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/create_parking_lot/{numberOfSlot}", createParkingSlot).Methods("POST")
-	r.HandleFunc("/park/{regNumber}/{colour}", reserve).Methods("POST")
-	r.HandleFunc("/leave/{slotNumber}", leave).Methods("POST")
-	r.HandleFunc("/status", status).Methods("GET")
-	r.HandleFunc("/cars_registration_numbers/colour/{colour}", getRegNumbersByColour).Methods("GET")
-	r.HandleFunc("/cars_slot/colour/{colour}", getReservedSlotsByColour).Methods("GET")
-	r.HandleFunc("/slot_number/car_registration_number/{regNumber}", getReservedSlotByRegNumber).Methods("GET")
+	r.HandleFunc("/create_parking_lot/{numberOfSlot}", handler.CreateParkingSlot).Methods("POST")
+	r.HandleFunc("/park/{regNumber}/{colour}", handler.Reserve).Methods("POST")
+	r.HandleFunc("/leave/{slotNumber}", handler.Leave).Methods("POST")
+	r.HandleFunc("/status", handler.Status).Methods("GET")
+	r.HandleFunc("/cars_registration_numbers/colour/{colour}", handler.GetRegNumbersByColour).Methods("GET")
+	r.HandleFunc("/cars_slot/colour/{colour}", handler.GetReservedSlotsByColour).Methods("GET")
+	r.HandleFunc("/slot_number/car_registration_number/{regNumber}", handler.GetReservedSlotByRegNumber).Methods("GET")
 
 	err := http.ListenAndServe(":8080", r)
 
